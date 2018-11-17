9411a2acebd35a4ad849e79d44ad85cd8e5efd1b
HananNamrouti <hanan.namrouti@gmail.com>
2018-11-17 12:22:06 +0200

    1

--- Ass2/src/math/TaxCalc.java
+++ Ass2/src/math/TaxCalc.java
@@ -23,7 +23,7 @@ public class TaxCalc {
         // A Reader stream to read from the console
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 
-        System.out.println ("Welcome to the new Palestine tax calculator.");
+        System.out.println ("Welcome to the new Palestine tax calculator Annually.");
         System.out.print   ("How much did you earn last year? ");
         try {
             income = Double.parseDouble(in.readLine());
@@ -42,14 +42,14 @@ public class TaxCalc {
         }
 //     check income
 
-        if (income < 0) {
+        if (income <= 0) {
             System.out.println ("We  can not calculate for 0 income !");
             System.out.println ("Start over.");
             print_tax(taxTotal );
         }
 
         
-        if (income < 1450) {
+        if (income <= 1450) {
             System.out.println ("Your Income is Equal or less than Minimum wage ");
             System.out.println ("Start over.");
             print_tax(taxTotal );
@@ -73,16 +73,16 @@ public class TaxCalc {
 //     check Your age      
        
        if (age <= 25) {
-           System.out.println("Did you Still Study?");
+           System.out.println("Are You Still Studing?");
            System.out.println ("Start over.");
            print_tax(taxTotal );
            }
 
 //    compute tax total
 
-      if (income < 1000)
+      if (income < 2000 && income > 1450)
          taxTotal = 0.12 * income;
-      else if (income < 5000)
+      else if (income < 5000 && income >= 2000)
          taxTotal = 300.00 + 0.24 * (income - 1000);
       else
          taxTotal = 1500.00 + 0.36 * (income - 5000);
